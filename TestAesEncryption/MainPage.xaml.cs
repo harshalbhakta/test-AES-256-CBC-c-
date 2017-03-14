f3ffe1d0888745b03c519794a46a2cb2cdaa6b12
Harshal Bhakta <harshal.c.bhakta@gmail.com>
2017-03-14 12:57:10 +0530

    Encryption works with dynamic keys

--- TestAesEncryption/MainPage.xaml.cs
+++ TestAesEncryption/MainPage.xaml.cs
@@ -26,16 +26,40 @@ namespace TestAesEncryption
         // Key with 256 and IV with 16 length
         private string AES_Key = "280f8bb8c43d532f389ef0e2a5321220b0782b065205dcdfcb8d8f02ed5115b9";
         private string AES_IV = "CC0A69779E15780ADAE46C45EB451A23";
-        string convertedKey = "KA+LuMQ9Uy84nvDipTISILB4KwZSBdzfy42PAu1RFbk=";
-        string convertedIV = "zAppd54VeAra5GxF60UaIw==";
+        private string convertedKey = "KA+LuMQ9Uy84nvDipTISILB4KwZSBdzfy42PAu1RFbk=";
+        private string convertedIV = "zAppd54VeAra5GxF60UaIw==";
         private IBuffer m_iv = null;
         private CryptographicKey m_key;
 
-        private string convertHexToBase64(string hexStringToConvert)
+        public static string AesEncrypt(string data, string hexKey, string hexIv)
         {
-            byte[] convertedByte = Encoding.Unicode.GetBytes(hexStringToConvert);
-            string hex = BitConverter.ToString(convertedByte);
-            return hex;
+            byte[] input = Encoding.ASCII.GetBytes(data);
+            string base64Key = Convert.ToBase64String(ConvertFromStringToHex(hexKey)); // "KA+LuMQ9Uy84nvDipTISILB4KwZSBdzfy42PAu1RFbk="
+            string base64IV = Convert.ToBase64String(ConvertFromStringToHex(hexIv));   // "zAppd54VeAra5GxF60UaIw==";
+
+            IBuffer key = Convert.FromBase64String(base64Key).AsBuffer();
+            IBuffer m_iv = Convert.FromBase64String(base64IV).AsBuffer();
+            SymmetricKeyAlgorithmProvider provider = SymmetricKeyAlgorithmProvider.OpenAlgorithm(SymmetricAlgorithmNames.AesCbcPkcs7);
+            CryptographicKey m_key = provider.CreateSymmetricKey(key);
+
+            IBuffer bufferMsg = CryptographicBuffer.ConvertStringToBinary(Encoding.ASCII.GetString(input), BinaryStringEncoding.Utf8);
+            IBuffer bufferEncrypt = CryptographicEngine.Encrypt(m_key, bufferMsg, m_iv);
+            byte[] encryptedBytes = bufferEncrypt.ToArray();
+
+            string result = Convert.ToBase64String(encryptedBytes);
+            return result;
+        }
+
+        public static byte[] ConvertFromStringToHex(string inputHex)
+        {
+            inputHex = inputHex.Replace("-", "");
+
+            byte[] resultantArray = new byte[inputHex.Length / 2];
+            for (int i = 0; i < resultantArray.Length; i++)
+            {
+                resultantArray[i] = Convert.ToByte(inputHex.Substring(i * 2, 2), 16);
+            }
+            return resultantArray;
         }
 
         public AesEnDecryption()
@@ -48,7 +72,6 @@ namespace TestAesEncryption
 
         public byte[] Encrypt(byte[] input)
         {
-
             IBuffer bufferMsg = CryptographicBuffer.ConvertStringToBinary(Encoding.ASCII.GetString(input), BinaryStringEncoding.Utf8);
             IBuffer bufferEncrypt = CryptographicEngine.Encrypt(m_key, bufferMsg, m_iv);
             return bufferEncrypt.ToArray();
@@ -77,7 +100,7 @@ namespace TestAesEncryption
             AesEnDecryption en = new AesEnDecryption();
             byte[] encryptedBytes = en.Encrypt(Encoding.ASCII.GetBytes(originalText));
             string uni = Convert.ToBase64String(encryptedBytes);
-            EncryptedTextTextBlock.Text = uni;
+            EncryptedTextTextBlock.Text = AesEnDecryption.AesEncrypt(originalText, "280f8bb8c43d532f389ef0e2a5321220b0782b065205dcdfcb8d8f02ed5115b9", "CC0A69779E15780ADAE46C45EB451A23");
         }
 
         private void Decrypt_Click(object sender, RoutedEventArgs e)
